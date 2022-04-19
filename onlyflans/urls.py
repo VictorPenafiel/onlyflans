b8107afc8ca991f15d6b5f14614642a4da058f9f
VictorPenafiel <victorpenafiel@gmail.com>
2022-04-18 22:04:02 -0400

    detalles

--- onlyflans/urls.py
+++ onlyflans/urls.py
@@ -14,12 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from web.views import indice, acerca, bienvenido
+from django.urls import path, include
+from web.views import indice, acerca, bienvenido, contacto, exito
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',indice, name="indice"),
+    path('/', indice, name="indice"),
     path('acerca', acerca, name="acerca"),
     path('bienvenido', bienvenido, name="bienvenido"),
+    path('contacto', contacto, name="contacto"),
+    path('exito', exito, name="exito"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- web/views.py
+++ web/views.py
@@ -1,11 +1,49 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Flan, ContactForm
+from .forms import ContactFormForm, ContactFormModelForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+
 def indice(request):
-    return render(request, 'index.html',{})
+    public_flans = Flan.objects.filter(is_private=False)
+    return render(
+        request,
+        'index.html',
+        {
+            'public_flans': public_flans
+        }
+)
+
 
 def acerca(request):
-    return render(request, 'about.html',{})
+    return render(request, 'about.html', {})
 
+@login_required
 def bienvenido(request):
-    return render(request, 'welcome.html',{})
+    private_flans = Flan.objects.filter(is_private=True)
+    return render(
+        request,
+        'welcome.html',
+        {
+            'private_flans': private_flans
+        }
+)
+
+
+def contacto(request):
+    if request.method == 'POST':
+        form = ContactFormModelForm(request.POST)
+        if form.is_valid():
+            contact_form = ContactForm.objects.create(**form.cleaned_data)
+            return HttpResponseRedirect('/exito')
+    else:
+        form = ContactFormModelForm()
+    return render(request, 'contactus.html', {'form': form})
+
+
+def exito(request):
+    return render(request, 'success.html', {})
+
